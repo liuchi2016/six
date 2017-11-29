f3dadf188f26c467b6dae986a44648ad0fc27c5a
liuchi <liuchi@os-easy.com>
2017-11-29 14:25:41 +0800

    up multiclient start 60s

--- loader/app/src/main/java/com/oseasy/mmc/loader/Loader.java
+++ loader/app/src/main/java/com/oseasy/mmc/loader/Loader.java
@@ -96,7 +96,7 @@ public class Loader extends Service {
                     @Override
                     public void run() {
                         try {
-                            Thread.sleep(15000);
+                            Thread.sleep(60000);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
